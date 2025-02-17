aedd6a96677e61a72a25266d0c4059b0a1c42eb6
hitesh95reddy <33710615+hitesh95reddy@users.noreply.github.com>
2025-02-17 23:58:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ col1.write("I love you more every day.")
 col2.write("I'm grateful for every moment we share.") 
 col3.write("May this day be filled with as much love and joy as you bring into our lives.:smile:")
 
-col1,col2,col3=st.columns([.5, 2, .5])
+col1,col2,col3=st.columns([.1, 2, .1])
 #col1.write("=================:smile:")
-col2.header("Happy Birthday:birthday:, My Beautiful Wife.!!! :partying_face:")
+col2.header("Happy Birthday, My Beautiful Wife.!!! :partying_face:")
 #col3.write(":smile:=================")
 col1, col2 ,col3, col4, col5 = st.columns([3, 4, 5,4,3])
 col1.image('gif1.gif') # This is the same as the above line, but I have the GIF saved in the same directory as the script.
