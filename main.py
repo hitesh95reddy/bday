146425433dcef15a443d72f0dc4433f8f815167a
hitesh95reddy <33710615+hitesh95reddy@users.noreply.github.com>
2025-02-17 23:49:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ st.title("Happy Birthday Thushu :partying_face::birthday::gift::champagne::smile
 #st.write("Here's a little something for you:")
 st.write("I love you more every day.")
 st.write("I'm grateful for every moment we share.") 
-st.write("Happy Birthday, My Beautiful Wife.!!!")
+st.write("May this day be filled with as much love and joy as you bring into our lives.:smile:")
+st.write("Happy Birthday, My Beautiful Wife.!!! :partying_face:")
 col1, col2 ,col3, col4, col5 = st.columns([3, 4, 5,4,3])
 col1.image('gif1.gif') # This is the same as the above line, but I have the GIF saved in the same directory as the script.
 col2.image('1.jpeg')
