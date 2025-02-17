fac03fc049fabf983e7f00ba2265ed669a779284
hitesh reddy <hitesh95reddy@gmail.com>
2025-02-17 23:41:01 +0530

    i

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import base64
+
+st.title("Happy Birthday Thushu :partying_face::birthday::gift::champagne::smile: !!!")
+st.write("Here's a little something for you:")
+col1, col2 ,col3, col4, col5 = st.columns([3, 4, 5,4,3])
+col1.image('gif1.gif') # This is the same as the above line, but I have the GIF saved in the same directory as the script.
+col2.image('1.jpeg')
+col3.image('2.jpeg')
+col4.image('3.jpeg')
+col5.image('gif2.gif')
+# file_ = open("./gif1.gif", "rb")
+# contents = file_.read()
+# data_url = base64.b64encode(contents).decode("utf-8")
+# file_.close()
+
+# st.markdown(
+#     f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+#     unsafe_allow_html=True,
+# )
